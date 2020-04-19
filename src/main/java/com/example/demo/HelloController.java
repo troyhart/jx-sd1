dcf018893125668e4eb7499137dd968050e25cbd
Troy Hart <thart@myriad.com>
2020-04-19 11:18:11 -0600

    ...

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,14 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
 
+    private static final List<String> greetings = new ArrayList<>();
+    {
+        greetings.add("hello");
+        greetings.add("bonjour");
+        greetings.add("hola");
+        greetings.add("howdy");
+    }
+
     /**
      * just saying hello.
      * 
      * @return simple greeting
      */
     @GetMapping()
-    public String hello() {
-        return "hello";
+    public String hello(@RequestParam(required = false, defaultValue = "0") int index) {
+        if (index < 0 || index >= greetings.size()) {
+            // set index to last element if given value is equal to the size of the list
+            // otherwise set the index to zero.
+            index = index == greetings.size() ? index - 1 : 0;
+        }
+        return greetings.get(index);
     }
-    
+
 }
